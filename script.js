37f584080128bbe92e3f39060cf9f869af7e016e
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-24 15:28:03 +0000

    loop for box print out column

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const rows = document.querySelector('#row');
 
-const box = document.createElement('div');
-box.classList.add('box');
-box.textContent = 'Box';
+function printGrid () {
+    for (let i = 0; i < 16; i++) {
+        const rows = document.querySelector('#row');
 
-rows.appendChild(box);
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.textContent = '';
+        
+        rows.appendChild(box);
+    }
+}
 
-// function printGrid () {
-
-//     for (let i = 0; i < 16; i++) {
-//         const div = document.createElement('div');
-//         div.classList.add('box');
-//         rows.appendChild('div');
-//     }
-// }
+printGrid();
