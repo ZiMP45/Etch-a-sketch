ad369d23c3442ce52643da123a7d4d5867dbd3b7
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-25 17:01:42 +0000

    fix row and column print out

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 
 function createGrid () {
-        const grid = document.querySelector('.grid')
-
-        const row = document.createElement('div');
-        row.classList.add('row');
-        row.textContent = '';
-
-        grid.appendChild(row);
-
-        for (let i = 0; i < 16; i++) {
-            const rows = document.querySelector('.row');
-
-            const box = document.createElement('div');
-            box.classList.add('box');
-            box.textContent = '';
-            
-            rows.appendChild(box);
+    for (let i = 0; i < 16; i++) {
+        createRows(i);
+        for (let j = 0; j < 16; j++) {
+            createColumns(i, j);
         }
+    }
 } 
+
+function createRows (i) {
+    const grid = document.querySelector('.grid');
+
+    const row = document.createElement('div');
+    row.classList.add('row' + i);
+    row.textContent = '';
+
+    grid.appendChild(row);
+}
+
+function createColumns (i, j) {
+
+    const rows = document.querySelector('.row' + i);
+
+    const box = document.createElement('div');
+    box.classList.add('box' + j);
+    box.textContent = '';
+    
+    rows.appendChild(box);
+}
 
 createGrid();
 
