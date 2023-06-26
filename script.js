e715ec168b57d6c2c14f6adfcfd0de5183881928
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-26 18:17:41 +0000

    re-write grid print functions, change color on hover

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
-createGrid(16, 16);
+const grid = document.querySelector('.grid');
 
-function getValue() {
-    var e = document.getElementById("gridSize");
-    printGrid(e);
+function createGrid () {
+    for (let i = 0; i < 256; i++){     
+        const row = document.createElement('div');
+        row.classList.add('box');
+        row.addEventListener('mouseover', e => e.target.classList.add('hover'));
+        grid.append(row);
+    }   
 }
 
-function printGrid(size) {
-
-    let height = size.value;
-    let width = size.value;
-
-    createGrid(height, width);
-}
-
-function createGrid (height, width) {
-    for (let i = 0; i < height; i++) {
-        createRows(i);
-        for (let j = 0; j < width; j++) {
-            createColumns(i, j);
-        }
+function deleteChild (parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
-} 
-
-function createRows (i) {
-    const grid = document.querySelector('.grid');
-
-    const row = document.createElement('div');
-    row.setAttribute('id', 'row' + i);
-    row.classList.add('yDimension');
-    row.textContent = '';
-
-    grid.appendChild(row);
 }
 
-function createColumns (i, j) {
 
-    const rows = document.querySelector('#row' + i);
+createGrid();
 
-    const box = document.createElement('div');
-    box.setAttribute('id', 'box' + j);
-    box.classList.add('xDimension');
-    box.textContent = '';
-    
-    rows.appendChild(box);
-}
+// const container = document.querySelector('.grid');
+// deleteChild(container);
